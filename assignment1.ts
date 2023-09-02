db76e67e17d1df3d78e9e5e6cf95f22efd18a25d
FahadUmairAhmed <fahadumair88@gmail.com>
2023-09-01 17:13:56 -0700

    while loop added

---
+++ assignment1.ts
@@ -0,0 +1,88 @@
+// //Create a funtion that takes an array,an index and a value as parameters.Inside function use the splice method to insert the value at the specified index in the array.Return the modified array.
+// let array:number[] = [1,2,3,4,5]
+// let index :number= 5;
+// let parameters: number =7;
+// function arrayindexparameters(array:number[] , index: number , parameters:number):number[] 
+// {
+//     array.splice(index,0,parameters)
+//     return array
+// }
+// var modifyarray:number[]=arrayindexparameters(array,index,parameters);
+// console.log(modifyarray);
+// //Write a program that uses a while loop to print the first 25 integers.
+// let count: number =1;
+// while (count<=25){
+//     console.log("First 25 integers",count);
+//     count++;
+// }
+//Write a program that uses a while loop to print the first 10 even number.
+// var num01:number=1;
+// while (num01<=10){
+//   if (num01%2==0)
+//   console.log(num01," ");
+//     num01++;
+// }
+//Implement simple shopping cart using an array.Create functions to add items,remove items,and update quantities using the splice method.Print the cart's content after each operation.
+// var shop:string[]=["Coke","Ice cream","Sprite","Lays"]
+// function additem(shop:string[]){
+//     var add= shop.splice(shop.length-3,0,"Apple");
+//     console.log(add)
+// }
+// console.log(shop)
+// console.log(shop,additem)
+//Create a fuction that takes an array of numbers as parameter.Use a while loop to calculate and return the sum of all the numbers in the array.
+// let num02=[1,2,3,4,5,6,7,8,9]
+// let num03=[7,8,9,6,5,7,8,9,7]
+// // console.log("Value of arrays: ",num02)
+// function addnum(num01:number[]) {
+//     let i=0,sum=0;
+//     while (i<num02.length);{
+//         sum=sum+num02[i];
+//         i++;
+//     }
+//   // return sum;  
+// }
+// console.log (addnum(num02))
+//Negative array remove program
+// const negativearray:number[]=[1,2,-3,-4,-5,6,7];
+// function removenegative(negativearray:number[]) {
+//   for (let i=negativearray.length;i>=0;i--){
+//     if (negativearray[i]<0) {
+//       negativearray.splice(i,1);
+//     }
+//   }  
+// }
+// removenegative(negativearray);
+// console.log(negativearray)
+//Function factorial
+// let facnum:number=20;
+// const factorial=(facnum:number)=>{
+//   if (facnum>0) {
+//     let result=1;
+//     let index=1;
+//     while(index<=facnum){
+//       result*=index;
+//       index++;
+//     }
+//     return result;
+//   }else{
+//     throw new Error("Enter Positive Number"); 
+//   }
+// }
+// let facresult:number=factorial(facnum);
+// console.log(facresult)
+//Temperature
+// let tempincel:number[]=[20,40,30];
+// const convertinf=(tempincel:number[]):number[]=>{
+//   const fahrentemp:number[]=[];
+//   let i=0;
+//   while (i<tempincel.length){
+//     const celsiuis=tempincel[i];
+//     const fahren=(celsiuis*9)/5+32;
+//     fahrentemp.push(fahren);
+//     i++;
+//   }
+// return fahrentemp;
+// }
+// let tempresult=convertinf(tempincel);
+// console.log(tempresult);
---
+++ assignment2.ts
@@ -0,0 +1,59 @@
+//Develop a program that calculates and prints the sum of the n even numbers using a for loop.
+let evennum = 11;
+function sumeven(evennum:number) {
+    let sum=0;
+    for(let i=1;i <= evennum;i++){   
+    }
+    return sum;
+}
+const result= sumeven(evennum);
+console.log((result));
+//Implement a program that uses a for loop to iterate through an array of numbers and print only the even numbers.
+let array= [1,2,3,4,5,6,7,8,9,10];
+for (let i = 0; i < array.length; i++) {
+    if(array[i]%2==0){
+        console.log(array[i]);
+    }   
+}
+//Implement a program that uses to iterate through an array of numnbers and remove all even numbers from them and just leave the odd ones.
+let array1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+for (let i = 0; i < array1.length; i++) {
+    if(array1[i]%2===0){
+        array1.splice(i,1);
+    }    
+}
+console.log(array1);
+//Write a program that defines a function to calculates the area of a circle.The function should take the radius as input and return the calculated area.
+let radius=12;
+const area=(radius:number) =>{
+    const pi= Math.PI;
+    let area1=pi*radius**2;
+    return area;
+};
+let circle=area(radius);
+console.log(circle);
+//Develop a program that reads a list of grades and uses the splice method to remove failing grade(below 50) from the array.
+let marks:number[]= [33,55,60,80,45,70,99,49,67];
+const removegrade=(grade:number[]) => {
+    for (let i = 0; i < marks.length; i++) {
+        if (marks[i]<50) {
+            marks.splice(i,1); 
+        }        
+    }
+    return marks;
+};
+let markresult= removegrade(marks);
+console.log(markresult);
+//Write a program that uses a function to find the largest element in an array of numbers.
+let array2: number[] =[10,15,20,25,30,35,40,45,50];
+const largenum=(value:number[]) => {
+    let large=value[0];
+    for (let i = 0; i < value.length; i++) {
+        if (value[i] > large) {
+            large= value[i];
+        }       
+    }
+    return large;
+};
+let largevalue= largenum(array2);
+console.log(largevalue);
